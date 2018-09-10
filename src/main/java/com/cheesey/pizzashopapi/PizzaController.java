b05d21eb8b5eda8a45dcb64e9d398aa07ff3ef8e
Barry Tarlton <btarlton@gmail.com>
2018-09-10 09:53:33 -0400

    initial commit of simple working api

---
+++ src/main/java/com/cheesey/pizzashopapi/PizzaController.java
@@ -0,0 +1,16 @@
+package com.cheesey.pizzashopapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PizzaController {
+	
+	@RequestMapping("/pizzas")
+	public String getPizzas() {
+		
+		return "Pizza are good";
+		
+	}
+
+}
